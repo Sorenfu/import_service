cca042fdd893d3e5c03c8b96af2477e8c33888ff
Victor <viktor.markevich@faceit.com.ua>
2017-09-12 16:01:28 +0300

    Add simple spec for sheet model

--- spec/factories/sheets.rb
+++ spec/factories/sheets.rb
@@ -1,11 +1,18 @@
 # coding: utf-8
 FactoryGirl.define do
-
   factory :sheet do
     pol 'test'
     pod 'test'
     rate_wm 1
     rate_min 1
     tt 1
+  end
+
+  factory :invalid_sheet, parent: :sheet do
+    pol nil
+    pod nil
+    rate_wm nil
+    rate_min nil
+    tt nil
   end
 end
--- spec/models/sheet_spec.rb
+++ spec/models/sheet_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Sheet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'check validations' do
+    context 'when valid' do
+      it 'has valid factory' do
+        expect(build :sheet).to be_valid
+      end
+    end
+
+    context 'when invalid' do
+      it 'has empty attrs' do
+        expect(build(:invalid_sheet)).to_not be_valid
+      end
+    end
+  end
 end
